c2491e84f4649bbc1eca7b225a1d61401af7d56d
AADad578 <52874739+AADad578@users.noreply.github.com>
2024-02-25 18:27:05 -0500

    Intake Subsystem
    Created Intake Subsystem and Intake Pivot Subsystem.
    
    Created Commands to control it and Inputs to control those.

--- src/main/java/frc/robot/Inputs.java
+++ src/main/java/frc/robot/Inputs.java
@@ -1,5 +1,37 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
 public class Inputs {
+    private static final CommandXboxController driver = new CommandXboxController(Constants.OperatorConstants.DriverController.port); 
+    private static final CommandXboxController operator = new CommandXboxController(Constants.OperatorConstants.OperatorController.port); 
+    private static final CommandGenericHID operatorBoard = new CommandGenericHID(Constants.OperatorConstants.OperatorButtonBoard.port); 
+
+
+    public static double getIntakePivotSpeed() {
+        return operator.getLeftX();
+    }
+
+    public static Trigger getIntakeFloorPos() {
+        return operatorBoard.button(1);
+    }
+
+    public static Trigger getIntakeBasePos() {
+        return operatorBoard.button(2);
+    }
+
+    public static Trigger getIntakeSpinIn() {
+        return operatorBoard.button(3);
+    }
+
+    public static Trigger getIntakeSpinOut() {
+        return operatorBoard.button(4);
+    }
+
+    public static Trigger getIntakeSpinStop() {
+        return operatorBoard.button(5);
+    }
     
 }
---
+++ src/main/java/frc/robot/MotorConfiguration.java
@@ -0,0 +1,191 @@
+package frc.robot;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.revrobotics.CANSparkBase;
+import com.revrobotics.CANSparkBase.IdleMode;
+
+public class MotorConfiguration {
+    private double P;
+    public double getP() {
+        return P;
+    }
+
+    public void setP(double p) {
+        P = p;
+    }
+
+    private double I;
+    public double getI() {
+        return I;
+    }
+
+    public void setI(double i) {
+        I = i;
+    }
+
+    private double D;
+    public double getD() {
+        return D;
+    }
+
+    public void setD(double d) {
+        D = d;
+    }
+
+    private double Iz;
+    public double getIz() {
+        return Iz;
+    }
+
+    public void setIz(double iz) {
+        Iz = iz;
+    }
+
+    private double F;
+    private double maxOutput;
+    private double minOutput;
+    private double gearRatio = 1;
+    private double position;
+    private int currentLimit;
+    private boolean idleCoast;
+    private boolean inverted;
+    private double openLoopRampRate;
+
+    public double getOpenLoopRampRate() {
+        return this.openLoopRampRate;
+    }
+
+    public void setOpenLoopRampRate(double openLoopRampRate) {
+        this.openLoopRampRate = openLoopRampRate;
+    }
+
+    public double getF() {
+        return this.F;
+    }
+
+    public void setF(double F) {
+        this.F = F;
+    }
+
+    public double getMaxOutput() {
+        return this.maxOutput;
+    }
+
+    public void setMaxOutput(double maxOutput) {
+        this.maxOutput = maxOutput;
+    }
+
+    public double getMinOutput() {
+        return this.minOutput;
+    }
+
+    public void setMinOutput(double minOutput) {
+        this.minOutput = minOutput;
+    }
+
+    public double getGearRatio() {
+        return this.gearRatio;
+    }
+
+    public void setGearRatio(double gearRatio) {
+        this.gearRatio = gearRatio;
+    }
+
+    public double getPosition() {
+        return this.position;
+    }
+
+    public void setPosition(double position) {
+        this.position = position;
+    }
+
+    public int getCurrentLimit() {
+        return this.currentLimit;
+    }
+
+    public void setCurrentLimit(int currentLimit) {
+        this.currentLimit = currentLimit;
+    }
+
+    public boolean isIdleCoast() {
+        return this.idleCoast;
+    }
+
+    public void setIdleCoast(boolean idleCoast) {
+        this.idleCoast = idleCoast;
+    }
+
+    public boolean isInverted() {
+        return this.inverted;
+    }
+
+    public void setInverted(boolean inverted) {
+        this.inverted = inverted;
+    }
+    
+    public MotorConfiguration(double P, double I, double D, double Iz, double F, double maxOutput, double minOutput, double gearRatio, double position, int currentLimit, boolean idleCoast, boolean inverted, double openLoopRampRate){
+        this.P = P;
+        this.I = I;
+        this.D = D;
+        this.Iz = Iz;
+        this.F = F;
+        this.maxOutput = maxOutput;
+        this.minOutput = minOutput;
+        this.gearRatio = gearRatio;
+        this.position = position;
+        this.currentLimit = currentLimit;
+        this.idleCoast = idleCoast;
+        this.inverted = inverted;
+        this.openLoopRampRate = openLoopRampRate;
+    }
+
+    public MotorConfiguration(double maxOutput, double minOutput, int currentLimit, boolean idleCoast, boolean inverted){
+        this.maxOutput = maxOutput;
+        this.minOutput = minOutput;
+        this.currentLimit = currentLimit;
+        this.idleCoast = idleCoast;
+        this.inverted = inverted;
+    }
+
+    public static void configureMotor(CANSparkBase motor, MotorConfiguration config){
+        motor.restoreFactoryDefaults();
+        var pid = motor.getPIDController();
+        pid.setP(config.getP());
+        pid.setI(config.getI());
+        pid.setD(config.getD());
+        pid.setIZone(config.getIz());
+        pid.setFF(config.getF());
+        pid.setOutputRange(config.getMinOutput(), config.getMaxOutput());
+        var encoder = motor.getEncoder();
+        encoder.setPositionConversionFactor(config.getGearRatio());
+        encoder.setPosition(config.getPosition());
+        motor.setSmartCurrentLimit(config.getCurrentLimit());
+        motor.setInverted(config.isInverted());
+        motor.setIdleMode(config.isIdleCoast() ? IdleMode.kCoast : IdleMode.kBrake);
+        motor.setOpenLoopRampRate(config.getOpenLoopRampRate());
+        motor.burnFlash();
+    }
+
+    public static TalonFXConfiguration configureMotor(TalonFX motor, MotorConfiguration config){
+        var c = motor.getConfigurator();
+        var configuration = new TalonFXConfiguration();
+        configuration.CurrentLimits.SupplyCurrentLimit = config.getCurrentLimit();
+        configuration.CurrentLimits.SupplyCurrentLimitEnable = true;
+        configuration.MotorOutput.Inverted = config.isInverted() ? InvertedValue.Clockwise_Positive : InvertedValue.CounterClockwise_Positive;
+        configuration.MotorOutput.NeutralMode = config.isIdleCoast() ?  NeutralModeValue.Coast : NeutralModeValue.Brake;
+        configuration.MotorOutput.PeakForwardDutyCycle = config.getMaxOutput();
+        configuration.MotorOutput.PeakReverseDutyCycle = config.getMinOutput();
+        configuration.OpenLoopRamps.DutyCycleOpenLoopRampPeriod = config.getOpenLoopRampRate();
+        configuration.Slot0.kP = config.getP();
+        configuration.Slot0.kI = config.getI();
+        configuration.Slot0.kD = config.getD();
+        configuration.Slot0.kG = config.getF();
+        configuration.Feedback.SensorToMechanismRatio = config.getGearRatio();
+        motor.setPosition(config.getPosition());
+        c.apply(configuration);
+        return configuration;
+    }
+}
