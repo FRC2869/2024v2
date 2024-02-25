2f6f897db8f977dac024fbc0c38bc0f20667dc38
AADad578 <52874739+AADad578@users.noreply.github.com>
2024-02-25 14:20:39 -0500

    initial commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,26 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Timer;
+
+public class Constants {
+    public static Timer timer = new Timer();
+    public static class OperatorConstants {
+        public static interface DriverController{
+            public static final int port = 0;
+            public static final double leftXDeadband = .01;
+            public static final double leftYDeadband = .01;
+            public static final double rightXDeadband = .01;
+            public static final double righYDeadband = .01;
+        }        
+        public static interface OperatorController{
+            public static final int port = 2;
+            public static final double leftXDeadband = .01;
+            public static final double leftYDeadband = .01;
+            public static final double rightXDeadband = .01;
+            public static final double righYDeadband = .01;
+        } 
+        public static interface OperatorButtonBoard{
+            public static final int port = 3;
+        } 
+    }
+}
