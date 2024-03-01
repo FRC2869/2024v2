e032c66ad84ab4d4c5f3df9615d56024a523c0e0
RegalEagles2869 <theregaleagles2869@gmail.com>
2024-02-29 19:50:56 -0500

    added ShooterSubsystem

--- src/main/java/frc/robot/Inputs.java
+++ src/main/java/frc/robot/Inputs.java
@@ -2,9 +2,26 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class Inputs {
     private static final CommandXboxController driver = new CommandXboxController(Constants.OperatorConstants.DriverController.port); 
     private static final CommandXboxController operator = new CommandXboxController(Constants.OperatorConstants.OperatorController.port); 
     private static final CommandGenericHID operatorBoard = new CommandGenericHID(Constants.OperatorConstants.OperatorButtonBoard.port); 
+
+    public static Trigger getShooterShoot(){
+        return operatorBoard.button(1);
+    }
+
+    public static Trigger getShooterAmpScore(){
+        return operatorBoard.button(2);
+    }
+
+    public static Trigger getShooterAmpLoad(){
+        return operatorBoard.button(3);
+    }
+
+    public static Trigger getShooterStop(){
+        return operatorBoard.button(4);
+    }
 }
